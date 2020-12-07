fc09abe93779bca792c2991c9fec61cd3a775fe4
shivy108 <shivy101@live.com>
2020-12-07 21:18:55 +0100

    url

--- backend/app/users/urls.py
+++ backend/app/users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from app.users.views import ListUsers, RetrieveUser, ListUpdateMeView
 
 urlpatterns = [
-    path('list/', ListUsers.as_view(), name='list-users'),
+    
     path('<int:user_id>/', RetrieveUser.as_view(), name='retrieve-user'),
     path('', ListUpdateMeView.as_view(), name='list-users'),
     path('', ListUsers.as_view()),
